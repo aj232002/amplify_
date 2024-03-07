632465ebc9af83bf45371e7ef21dd6fe394cf94f
Jasmol <jarora20@myseneca.ca>
2024-03-07 19:10:48 +0530

    Third

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplify26a9fef3": {
       "AppClientID": "string",
